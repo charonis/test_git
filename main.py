7cad0f8897547e664b76fa9fab47f8c46ede510a
charon <charon@CHmail.com>
2024-03-18 23:22:02 +0300

    new project

---
+++ main.py
@@ -0,0 +1,15 @@
+def myPlass(x:int , y: int ):
+    return x +  y 
+
+
+
+def myMunus(x: int , y: int):
+    return x - y
+
+
+def devoidet(x: int , y: int ):
+    return x / y
+
+    
+print( myPlass(5, 10))
+
